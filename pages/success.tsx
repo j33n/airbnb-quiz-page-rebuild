d509ffdeb61bfc1b0f41cdbcf234309b6491e0f6
Jean Abayo <jabayo@Jeans-MacBook-Pro.local>
2022-05-01 04:02:23 +0200

    render data selected in forms

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import {Header} from 'components'
+import { Header } from "components";
 
-import {QuizType} from 'types'
+import { QuizType, QuizResponseType } from "types";
 
-import quizData from 'data/quiz'
+import quizData from "data/quiz";
 
 import {
   MainContainerStyle,
@@ -14,15 +14,38 @@ import {
   ContainerResultStyled,
   QuestionStyled,
   AnswerStyled,
-} from 'styles/SuccessStyle'
+} from "styles/SuccessStyle";
+
+export type DataToRenderType = {
+  question: string;
+  answer: string;
+};
 
 const Success = () => {
-  const [resDataFetched, setResDataFetched] = useState([])
+  const [resDataFetched, setResDataFetched] = useState<DataToRenderType[]>([]);
 
   useEffect(() => {
-    const quizData: any = JSON.parse(localStorage.getItem('quizData') || "")
-    setResDataFetched(quizData)
-  }, [])
+    const quizResult: QuizResponseType[] = JSON.parse(
+      localStorage.getItem("quizData") || ""
+    );
+    formalizeData(quizResult, quizData);
+  }, []);
+
+  const formalizeData = (
+    quizResult: QuizResponseType[],
+    quizData: QuizType
+  ) => {
+    const result: DataToRenderType[] = [];
+    quizResult.map((res: QuizResponseType) => {
+      result.push({
+        question: quizData[res.answerId - 1]?.title,
+        answer:
+          quizData[res.answerId - 1]?.answers[res.responseId - 1]?.description,
+      });
+    });
+
+    setResDataFetched(result);
+  };
 
   return (
     <Container>
@@ -30,24 +53,20 @@ const Success = () => {
         <HeaderContentStyled>
           <Header
             title="HomeAppeal Renovation Results"
-            description="Here are the results for the quiz:"
+            description="Results for the quiz:"
           />
         </HeaderContentStyled>
       </MainContainerStyle>
       <ResultsBoxStyled>
-        {resDataFetched?.map((val: any, key) => (
-          <ContainerResultStyled key={val.answerId}>
-            {/* TODO: adjust data to render valid selected qns */}
-            {/* {console.log(val)} */}
-            <QuestionStyled>{quizData[key].title}</QuestionStyled>
-            <AnswerStyled>
-              {quizData[key].answers[1].description}
-            </AnswerStyled>
+        {resDataFetched.map((res: DataToRenderType, key) => (
+          <ContainerResultStyled key={res.question + key}>
+            <QuestionStyled>{res.question}</QuestionStyled>
+            <AnswerStyled>{res.answer}</AnswerStyled>
           </ContainerResultStyled>
         ))}
       </ResultsBoxStyled>
     </Container>
-  )
-}
+  );
+};
 
-export default Success
+export default Success;
